fce95b1f7083eb7516a559b907e722adbb9992de
thuanvoviet <34815358+thuanvoviet@users.noreply.github.com>
2023-04-25 14:50:52 +0700

    add admin services

---
+++ public/js/services/admin/book-browsing.js
@@ -0,0 +1,8 @@
+// --------------------- dom action --------------------
+
+// ------------------- socket comm. ------------------
+
+
+;[].forEach(socket => socketCleanUp.push(socket))
+
+socket.emit('no_action')
